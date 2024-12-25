f45e8b7e7a8bc1c1a1710e6557a223780a5f36eb
Inncent <innokentiykim90@gmail.com>
2024-12-25 21:38:37 +0500

    Todo initial commit

---
+++ ToDoList_FastAPI/app/schema.py
@@ -0,0 +1,45 @@
+from datetime import datetime
+from pydantic import BaseModel
+from typing import Literal
+
+
+class IdResponseBase(BaseModel):
+    id : int
+
+class GetTodoResponse(BaseModel):
+    id: int
+    title: str
+    description: str
+    important: bool
+    done: bool
+    start_time: datetime
+    end_time: datetime | None
+
+
+class CreateTodoRequest(BaseModel):
+    title: str
+    description: str
+    important: bool
+
+
+class CreateTodoResponse(IdResponseBase):
+    pass
+
+
+class UpdateTodoRequest(BaseModel):
+    title: str | None = None
+    description: str | None = None
+    important: bool | None = None
+    done: bool | None = None
+
+
+class UpdateTodoResponse(IdResponseBase):
+    pass
+
+
+class StatusResponse(BaseModel):
+    status: Literal['deleted']
+
+
+class DeleteTodoResponse(StatusResponse):
+    pass
